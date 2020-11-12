e9ab1d448fc79ebbe2c9de7168396c3db17d69f4
remziusta <remziusta.mdbf15@iste.edu.tr>
2020-11-12 17:46:12 +0300

    Added navbar&sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmsModule } from './ems/ems.module';
 
 const routes: Routes = [];
 
--- src/app/ems/ems.module.ts
+++ src/app/ems/ems.module.ts
@@ -1,15 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-
+import { IgxNavbarModule,IgxRippleModule,IgxToggleModule,IgxButtonModule, IgxIconModule, IgxNavigationDrawerModule} from 'igniteui-angular';
 
 
 @NgModule({
   declarations: [NavbarComponent, SidebarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IgxNavbarModule,
+    IgxNavigationDrawerModule,
+    IgxIconModule,
+    IgxButtonModule,
+    IgxRippleModule,
+    IgxToggleModule
   ],
   exports: [NavbarComponent,SidebarComponent]
 })
-export class EmsModule { }
+export class EmsModule {
+
+
+  public ngOnInit() {  }
+
+}
